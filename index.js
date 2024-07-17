ce39c46a2f62bdf939b30dd97985d269ab847752
shivam jain <122403035+shivamjain1411@users.noreply.github.com>
2024-07-17 10:40:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const fs = require("fs");
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdfe45we45w345wegw345werjktjwertkj";
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
